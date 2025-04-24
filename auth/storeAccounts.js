2ac5d146fc6781c7a989fd7addaabd808ae2977f
TheRealDuckers <y24ducklingn@perins.hants.sch.uk>
2025-04-24 17:47:23 +0100

    Update storeAccounts.js

--- auth/storeAccounts.js
+++ auth/storeAccounts.js
@@ -1,29 +1,23 @@
 // Hash function using SHA-256
-async function hashPassword(password, salt) {
+async function hashPassword(password) {
     const encoder = new TextEncoder();
-    const data = encoder.encode(password + salt);
+    const data = encoder.encode(password); // Removed salt
     const hashBuffer = await crypto.subtle.digest("SHA-256", data);
     return Array.from(new Uint8Array(hashBuffer)).map(byte => byte.toString(16).padStart(2, "0")).join("");
-}
-
-// Generate a random salt
-function generateSalt() {
-    return Math.random().toString(36).substring(2, 10);
 }
 
 // Store predefined hashed accounts securely
 async function storeAccounts() {
     const predefinedAccounts = [
-        { username: "test@jeffai", password: "1234", salt: "randomSalt" },
-        { username: "user2", password: "mypassword", salt: "randomSalt" },
-        { username: "user3", password: "helloWorld!", salt: "randomSalt" },
-        { username: "user4", password: "JeffAIrocks", salt: "randomSalt" },
-        { username: "user5", password: "superSecret", salt: "randomSalt" }
+        { username: "test@jeffai", password: "1234" },
+        { username: "user2", password: "mypassword" },
+        { username: "user3", password: "helloWorld!" },
+        { username: "user4", password: "JeffAIrocks" },
+        { username: "user5", password: "superSecret" }
     ];
 
     for (let account of predefinedAccounts) {
-        account.salt = generateSalt();
-        account.password = await hashPassword(account.password, account.salt);
+        account.password = await hashPassword(account.password); // Removed salt logic
     }
 
     localStorage.setItem("accounts", JSON.stringify(predefinedAccounts));
